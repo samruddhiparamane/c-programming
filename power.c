94d3749c6fb011cdbfd108c256ff140907fea0c8
samruddhi paramane <smaruddhiparamane21@gmail.com>
2025-06-29 19:21:26 +0530

    first commit

--- power.c
+++ power.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    
+   int i,x,y,result=1;
+   printf("enter a number");
+   scanf("%d",&x);
+   printf("enter a power");
+   scanf("%d",&y);
+   i=1;
+   while(i<=y)
+   {
+    result=result*x;
+    i++;
+   }
+   printf("result is %d",result);
+   return 0;
+
 }
