69297a3937a9007c441e29236a663016a72de25f
samruddhi paramane <smaruddhiparamane21@gmail.com>
2025-07-05 00:25:19 +0530

    first commit

---
+++ sumof1ton.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n ,i,sum=0;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    sum=sum+i;
+    printf("sum is %d",sum);
+    return 0;     
+}
